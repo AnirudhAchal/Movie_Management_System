e201e9b748315c12a075b2aaea23ec6bdd2148bc
Anirudh Achal <anirudh.achal@gmail.com>
2021-03-06 12:17:02 +0530

    dummy posts added to templates

--- Movie_Management_System/dashboard/views.py
+++ Movie_Management_System/dashboard/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
 
+# Dummy movies
+posts = [
+    {
+        'director': 'Anirudh Achal',
+        'title': 'My life',
+        'description': 'This the description of the movie my life',
+        'date_posted': 'January 19th, 2021'
+    },
+    {
+        'director': 'Jk Rowling',
+        'title': 'Harry Potter',
+        'description': 'This the description of the movie harry potter',
+        'date_posted': 'January 29th, 2021'
+    }
+]
+
 # Create your views here.
 def home(request):
-    return render(request, 'dashboard/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'dashboard/home.html', context)
 
 
 def about(request):
