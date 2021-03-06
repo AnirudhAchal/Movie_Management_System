89d57199042362f80f97289b4a8d1d7567f62a29
Anirudh Achal <anirudh.achal@gmail.com>
2021-03-06 11:59:36 +0530

    Basic Templated added

--- Movie_Management_System/dashboard/views.py
+++ Movie_Management_System/dashboard/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
@@ -7,4 +6,4 @@ def home(request):
 
 
 def about(request):
-    return HttpResponse('<h1>Dashboard About</h1>')
+    return render(request, 'dashboard/about.html')
