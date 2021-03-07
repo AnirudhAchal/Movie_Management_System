36e2e13dcf1bea692dadf2e62fa8d940a0edf760
Anirudh Achal <anirudh.achal@gmail.com>
2021-03-07 16:53:34 +0530

    Movie model added

--- Movie_Management_System/dashboard/models.py
+++ Movie_Management_System/dashboard/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    language = models.CharField(max_length=20)
+    release_date = models.DateTimeField(default=timezone.now)
+    genre = models.CharField(max_length=20)
+    pg_rating = models.IntegerField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
--- Movie_Management_System/dashboard/views.py
+++ Movie_Management_System/dashboard/views.py
@@ -1,25 +1,10 @@
 from django.shortcuts import render
-
-# Dummy movies
-posts = [
-    {
-        'director': 'Anirudh Achal',
-        'title': 'My life',
-        'description': 'This the description of the movie my life',
-        'date_posted': 'January 19th, 2021'
-    },
-    {
-        'director': 'Jk Rowling',
-        'title': 'Harry Potter',
-        'description': 'This the description of the movie harry potter',
-        'date_posted': 'January 29th, 2021'
-    }
-]
+from .models import Movie
 
 # Create your views here.
 def home(request):
     context = {
-        'posts': posts
+        'movies': Movie.objects.all()
     }
     return render(request, 'dashboard/home.html', context)
 
