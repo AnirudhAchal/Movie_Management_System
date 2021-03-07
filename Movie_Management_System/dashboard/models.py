3e9e64160f9b317f75324001bf644c62a065b016
Anirudh Achal <anirudh.achal@gmail.com>
2021-03-07 17:11:10 +0530

    Duration field added to Movie model

--- Movie_Management_System/dashboard/models.py
+++ Movie_Management_System/dashboard/models.py
@@ -11,6 +11,7 @@ class Movie(models.Model):
     genre = models.CharField(max_length=20)
     pg_rating = models.IntegerField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    duration = models.IntegerField()
 
     def __str__(self):
         return self.title
