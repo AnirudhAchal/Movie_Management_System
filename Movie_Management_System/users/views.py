70a98c924578ec965695eef740274cdcecfe1c39
Rakshit <rakki22k@gmail.com>
2021-03-09 02:13:36 +0530

    change url to login page

--- Movie_Management_System/users/views.py
+++ Movie_Management_System/users/views.py
@@ -10,8 +10,8 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-            return redirect('dashboard-home')
+            messages.success(request, f'Your account has been created! You are now able to log in')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
