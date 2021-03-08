fde4bf738abe003206032ebea70e9668a435ce1d
Rakshit <rakki22k@gmail.com>
2021-03-09 01:51:51 +0530

    Add userProfile model
    Added user profile model. Also, added a media to store profile pics.

--- Movie_Management_System/Movie_Management_System/urls.py
+++ Movie_Management_System/Movie_Management_System/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from users import views as user_views
 from django.contrib.auth import views as auth_views
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,3 +28,6 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('', include('dashboard.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
