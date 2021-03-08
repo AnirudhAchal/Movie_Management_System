9ee083217ae7b49d221774df7fa8892954c88a20
Anirudh Achal <anirudh.achal@gmail.com>
2021-03-08 17:58:34 +0530

    bug fixed

---
+++ Movie_Management_System/Movie_Management_System/databaseconfig_example.py
@@ -0,0 +1,8 @@
+mysql = {
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': 'movie_management_system',
+    'USER': 'username',
+    'PASSWORD': 'password',
+    'HOST': 'localhost',
+    'POST': '3306',
+}
