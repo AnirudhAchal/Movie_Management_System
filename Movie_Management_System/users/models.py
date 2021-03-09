f18b516b23d3c32d031471a63574a3a0ace1a2bc
Anirudh Achal <anirudh.achal@gmail.com>
2021-03-09 14:04:03 +0530

    Bug fixed

--- Movie_Management_System/users/models.py
+++ Movie_Management_System/users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 
 class Profile(models.Model):
@@ -9,8 +10,8 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} Profile'
 
-    def save(self):
-        super().save()
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
 
         img = Image.open(self.image.path)
 
--- Movie_Management_System/users/signals.py
+++ Movie_Management_System/users/signals.py
@@ -8,6 +8,7 @@ from .models import Profile
 def create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
+        instance.profile.save()
 
 
 @receiver(post_save, sender=User)
