18fac07c30b1649f8a1575b077d9c53a7f5abf46
Anirudh Achal <anirudh.achal@gmail.com>
2021-03-09 14:08:00 +0530

    Bug Fixed

--- Movie_Management_System/users/signals.py
+++ Movie_Management_System/users/signals.py
@@ -8,7 +8,6 @@ from .models import Profile
 def create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
-        instance.profile.save()
 
 
 @receiver(post_save, sender=User)
