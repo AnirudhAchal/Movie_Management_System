99868fa9e4c5690802ffd72e74769381a713bd43
Anirudh Achal <anirudh.achal@gmail.com>
2021-03-19 16:24:42 +0530

    Basic language filter added

--- Movie_Management_System/dashboard/views.py
+++ Movie_Management_System/dashboard/views.py
@@ -3,9 +3,20 @@ from .models import Movie
 
 # Create your views here.
 def home(request):
+    if request.method == 'GET':
+        if len(request.GET.getlist('language')) == 0 or 'other' in request.GET.getlist('language'):
+            LANGUAGES = "(SELECT language FROM dashboard_movie)"
+
+        else:
+            LANGUAGES = request.GET.getlist('language').__str__().replace('[', '(').replace(']', ')')
+
+    movie_query = f'SELECT * FROM dashboard_movie WHERE language IN {LANGUAGES}'
+
     context = {
-        'movies': Movie.objects.all()
+        # 'movies': Movie.objects.all()
+        'movies': Movie.objects.raw(movie_query)
     }
+
     return render(request, 'dashboard/home.html', context)
 
 
