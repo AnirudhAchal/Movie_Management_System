6807b7ee9970914e966005efbf6eb9b954c58d0e
Anirudh Achal <anirudh.achal@gmail.com>
2021-03-19 22:16:21 +0530

    Duration filter added

--- Movie_Management_System/dashboard/views.py
+++ Movie_Management_System/dashboard/views.py
@@ -12,14 +12,25 @@ def home(request):
         else:
             LANGUAGES = request.GET.getlist('language').__str__().replace('[', '(').replace(']', ')')
 
+        # Get genre
         if len(request.GET.getlist('genre')) == 0: # No genre selected
             GENRES = "(SELECT genre FROM dashboard_movie)"
 
         else:
             GENRES = request.GET.getlist('genre').__str__().replace('[', '(').replace(']', ')')
 
+        # Get max duration
+        if request.GET.get('duration'):
+            DURATION = request.GET.get('duration')
+        else:
+            DURATION = "(SELECT MAX(duration) FROM dashboard_movie)"
 
-    movies_query = f'SELECT * FROM dashboard_movie WHERE language IN {LANGUAGES} AND genre in {GENRES}'
+
+    movies_query = f"""
+                SELECT * FROM dashboard_movie
+                WHERE language IN {LANGUAGES}
+                AND genre in {GENRES}
+                AND duration <= {DURATION}"""
     languages_query = f'SELECT DISTINCT 1 as id, language FROM dashboard_movie'
     genres_query = f'SELECT DISTINCT 1 as id, genre FROM dashboard_movie'
 
@@ -27,10 +38,8 @@ def home(request):
         # 'movies': Movie.objects.all()
         'movies': Movie.objects.raw(movies_query),
         'languages': Movie.objects.raw(languages_query),
-        'genres': Movie.objects.raw(genres_query)
+        'genres': Movie.objects.raw(genres_query),
     }
-
-    print(context['languages'])
 
     return render(request, 'dashboard/home.html', context)
 
