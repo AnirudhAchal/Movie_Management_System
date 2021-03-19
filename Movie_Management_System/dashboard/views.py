edddfb40b28aea7430e07ea998da67ca82d63ceb
Anirudh Achal <anirudh.achal@gmail.com>
2021-03-20 00:01:39 +0530

    Filter completed

--- Movie_Management_System/dashboard/views.py
+++ Movie_Management_System/dashboard/views.py
@@ -25,20 +25,41 @@ def home(request):
         else:
             DURATION = "(SELECT MAX(duration) FROM dashboard_movie)"
 
+        # Get max pg_rating
+        if request.GET.get('pg_rating'):
+            PG_RATING = request.GET.get('pg_rating')
+        else:
+            PG_RATING = "(SELECT MAX(pg_rating) FROM dashboard_movie)"
 
-    movies_query = f"""
-                SELECT * FROM dashboard_movie
-                WHERE language IN {LANGUAGES}
-                AND genre in {GENRES}
-                AND duration <= {DURATION}"""
+        # Get order_by
+        if request.GET.get('order_by'):
+            ORDER_BY = request.GET.get('order_by')
+        else:
+            ORDER_BY = "release_date"
+
+        # Get desc
+        if request.GET.get('order_by'):
+            DESC = "DESC"
+        else:
+            DESC = ""
+
     languages_query = f'SELECT DISTINCT 1 as id, language FROM dashboard_movie'
     genres_query = f'SELECT DISTINCT 1 as id, genre FROM dashboard_movie'
+    pg_ratings_query = f'SELECT DISTINCT 1 as id, pg_rating FROM dashboard_movie ORDER BY pg_rating'
+    movies_query = f"""SELECT * FROM dashboard_movie
+                        WHERE language IN {LANGUAGES}
+                        AND genre in {GENRES}
+                        AND duration <= {DURATION}
+                        AND pg_rating <= {PG_RATING}
+                        ORDER BY {ORDER_BY}
+                        {DESC}"""
 
     context = {
         # 'movies': Movie.objects.all()
         'movies': Movie.objects.raw(movies_query),
         'languages': Movie.objects.raw(languages_query),
         'genres': Movie.objects.raw(genres_query),
+        'pg_ratings': Movie.objects.raw(pg_ratings_query)
     }
 
     return render(request, 'dashboard/home.html', context)
