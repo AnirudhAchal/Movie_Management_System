588290dc7a5fd00f53ec78ac086ac96c5c76c630
Anirudh Achal <anirudh.achal@gmail.com>
2021-03-26 02:19:12 +0530

    Requirements.txt updated

--- Movie_Management_System/dashboard/models.py
+++ Movie_Management_System/dashboard/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django_mysql.models import EnumField
 
 # Create your models here.
 class Movie(models.Model):
@@ -15,3 +16,44 @@ class Movie(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class City(models.Model):
+    name = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    zipcode = models.CharField(max_length = 20)
+
+    def __str__(self):
+        return self.name
+
+
+class Cinema(models.Model):
+    name = models.CharField(max_length=100)
+    total_cinema_halls = models.IntegerField()
+    city_id = models.ForeignKey(City, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class CinemaHall(models.Model):
+    name = models.CharField(max_length=100)
+    total_seats = models.IntegerField()
+    cinema_id = models.ForeignKey(Cinema, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class CinemaSeat(models.Model):
+    seat_number = models.IntegerField()
+    type = EnumField(choices=['Basic', 'Executive', 'Premium', 'VIP'])
+    cinemal_hall_id = models.ForeignKey(CinemaHall, on_delete=models.CASCADE)
+
+
+"""
+class Show(models.Model):
+    date = models.DateField()
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+"""
