d9a3d26e13be89bb4fadbf748c2ae9f7c99b1333
Anirudh Achal <anirudh.achal@gmail.com>
2021-03-26 02:37:07 +0530

    Models added

--- Movie_Management_System/dashboard/models.py
+++ Movie_Management_System/dashboard/models.py
@@ -30,7 +30,7 @@ class City(models.Model):
 class Cinema(models.Model):
     name = models.CharField(max_length=100)
     total_cinema_halls = models.IntegerField()
-    city_id = models.ForeignKey(City, on_delete=models.CASCADE)
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
@@ -39,21 +39,35 @@ class Cinema(models.Model):
 class CinemaHall(models.Model):
     name = models.CharField(max_length=100)
     total_seats = models.IntegerField()
-    cinema_id = models.ForeignKey(Cinema, on_delete=models.CASCADE)
+    cinema = models.ForeignKey(Cinema, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
 
+
 class CinemaSeat(models.Model):
     seat_number = models.IntegerField()
     type = EnumField(choices=['Basic', 'Executive', 'Premium', 'VIP'])
-    cinemal_hall_id = models.ForeignKey(CinemaHall, on_delete=models.CASCADE)
+    cinema_hall = models.ForeignKey(CinemaHall, on_delete=models.CASCADE)
 
 
-"""
 class Show(models.Model):
     date = models.DateField()
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-"""
+    cinema_hall = models.ForeignKey(CinemaHall, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+
+
+class Booking(models.Model):
+    number_of_seats = models.IntegerField()
+    time_stamp = models.DateTimeField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    show = models.ForeignKey(Show, on_delete=models.CASCADE)
+
+
+class ShowSeat(models.Model):
+    price = models.FloatField()
+    cinema_seat = models.ForeignKey(CinemaSeat, on_delete=models.CASCADE)
+    show = models.ForeignKey(Show, on_delete=models.CASCADE)
+    booking = models.ForeignKey(Booking, on_delete=models.CASCADE)
