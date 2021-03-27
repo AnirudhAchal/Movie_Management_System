fbd385b78206bc26a708dc1bdb5af1a0c23a2ceb
Nanda Kishore KH <nandakishore323@gmail.com>
2021-03-28 01:07:30 +0530

    Fixed Error

--- Movie_Management_System/dashboard/urls.py
+++ Movie_Management_System/dashboard/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('', views.home, name='dashboard-home'),
     path('movie/<int:pk>/', MovieDetailView.as_view(), name='movie-detail'),
     path('about/', views.about, name='dashboard-about'),
-    path('search_bar/',views.search_bar,name = "search_bar")
+    path('search_bar/',views.search_bar,name = "search_bar"),
     path('movie/<int:pk>/book', views.book, name='movie-booking'),
 ]
