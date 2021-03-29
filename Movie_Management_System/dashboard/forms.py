9b5a9f92703d3c77fe720633de2acbc1f6fe0e34
Nanda Kishore KH <nandakishore323@gmail.com>
2021-03-29 15:53:24 +0530

    Disable Book Button for UnAuth Users

--- Movie_Management_System/dashboard/forms.py
+++ Movie_Management_System/dashboard/forms.py
@@ -1,9 +1,14 @@
 from django import forms
 from django.contrib.auth.models import User
-from .models import Booking
+from .models import Booking,Show
 
 class BookingForm(forms.ModelForm):
     class Meta:
         model = Booking
-        exclude = ['user']
+        fields = ('number_of_seats', 'show')
+
+        def __init__(self,movie_id,*args, **kwargs):
+            bruhhh,fuck,this,shit,lmao
+            super(BookingForm, self).__init__(*args, **kwargs)
+            self.fields['show'].queryset = Show.objects.get(movie_id=movie_id)
 
